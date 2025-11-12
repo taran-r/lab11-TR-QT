acf8c2696ed7f9003da30f533c3e2b8c5d8c02eb
WahniaHub <Quochuu2006@gmail.com>
2025-11-12 17:11:21 -0500

    new changes

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,20 @@
+# https://github.com/taran-r/lab11-TR-QT
+# Partner: Taran Raj
+# Partner: Quocnghia Truong
 import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    # Partner 2
+    def test_add(self):
+        self.assertTrue(add(1, 2) == 3)
+        self.assertTrue(add(-1, -2) == -3)
+        self.assertTrue(add(0, 0) == 0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertTrue(sub(1, -1) == 2)
+        self.assertTrue(sub(-10, -1) == -9)
+        self.assertTrue(sub(-10, 5) == -15)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +25,18 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):
+        self.assertTrue(log(2, 32) == 5)
+        self.assertTrue(log(2, 2) == 1)
+        self.assertTrue(log(2, 1) == 0)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            log(2, 0)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
